2424c81095feb9c4d27dc40381ef4c37b5966389
Jānis <jaanis.pavlovs@gmail.com>
2019-09-21 11:07:38 +0300

    no message

---
+++ overrideAutodetect.lua
@@ -0,0 +1,8 @@
+FullscreenEnabled = 0
+ResolutionWidth = 1920
+ResolutionHeight = 1080
+AntiAliasingDeferred = 2
+OverallGraphicsQuality = 4
+VSyncEnabled = 0
+PresentInterval = 1
+WindowedBorderless = 1
